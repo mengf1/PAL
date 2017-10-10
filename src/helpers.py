b268da6157d31f9fd2d42b3f73b4ce25f1028904
Moe <moefang@gmail.com>
2017-10-10 16:15:46 +1100

    Refine

--- src/helpers.py
+++ src/helpers.py
@@ -6,37 +6,35 @@ import tensorflow as tf
 
 # map a label to a string
 label2str = {1: "PER", 2: "LOC", 3: "ORG", 4: "MISC", 5: "O"}
-# load original data
 
+# predefine a label_set: PER - 1, LOC - 2, ORG - 3, MISC - 4, O - 5
+# 0 is for padding
+labels_map = {'B-ORG': 3, 'O': 5, 'B-MISC': 4, 'B-PER': 1, 'I-PER': 1, 'B-LOC': 2, 'I-ORG': 3, 'I-MISC': 4, 'I-LOC': 2}
 
-def load_data2labels(inputFile):
-    # predefine a label_set: PER - 1 LOC - 2 ORG - 3 MISC - 4 O - 5
-    # 0 is for padding
-    label_map = {'B-ORG': 3, 'O': 5, 'B-MISC': 4, 'B-PER': 1,
-                 'I-PER': 1, 'B-LOC': 2, 'I-ORG': 3, 'I-MISC': 4, 'I-LOC': 2}
-    seqs = []
+def load_data2labels(input_file):
+    seq_set = []
     seq = []
-    seqs_label = []
+    seq_set_label = []
     seq_label = []
-    seqs_len = []
-    with open(inputFile, "r") as f:
+    seq_set_len = []
+    with open(input_file, "r") as f:
         for line in f:
             line = line.strip()
             if line == "":
-                seqs.append(" ".join(seq))
-                seqs_label.append(seq_label)
-                seqs_len.append(len(seq_label))
+                seq_set.append(" ".join(seq))
+                seq_set_label.append(seq_label)
+                seq_set_len.append(len(seq_label))
                 seq = []
                 seq_label = []
             else:
                 tok, label = line.split()
                 seq.append(tok)
-                seq_label.append(label_map[label])
-    return [seqs, seqs_label, seqs_len]
+                seq_label.append(labels_map[label])
+    return [seq_set, seq_set_label, seq_set_len]
 
 
-def load_crosslingual_embeddings(inputFile, vocab, max_vocab_size=20000):
-    embeddings = list(open(inputFile, "r").readlines())
+def load_crosslingual_embeddings(input_file, vocab, max_vocab_size=20000):
+    embeddings = list(open(input_file, "r").readlines())
     pre_w2v = {}
     emb_size = 0
     for emb in embeddings:
@@ -54,11 +52,11 @@ def load_crosslingual_embeddings(inputFile, vocab, max_vocab_size=20000):
             w = ":"
         else:
             w = w_parts[1]
-        vals = []
+        vec = []
         for i in range(1, len(parts)):
-            vals.append(float(parts[i]))
-        # print w, vals
-        pre_w2v[w] = vals
+            vec.append(float(parts[i]))
+        # print w, vec
+        pre_w2v[w] = vec
 
     n_dict = len(vocab._mapping)
     vocab_w2v = [None] * n_dict
